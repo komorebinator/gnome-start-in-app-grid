ea9377c5d4ab6a7621726915e38c9db037adfb71
komorebi <markovs.i.mail@gmail.com>
2025-11-27 15:37:16 +0400

    Wait for startup-complete signal if still starting up

--- gnome-start-in-app-grid@borshevik/extension.js
+++ gnome-start-in-app-grid@borshevik/extension.js
@@ -3,6 +3,15 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class StartInAppGridExtension extends Extension {
     enable() {
-        Main.overview.showApps();
+        if (Main.layoutManager._startingUp) {
+            let startupCompleteId = Main.layoutManager.connect(
+                "startup-complete",
+                () => {
+                    Main.overview.showApps()
+                    Main.layoutManager.disconnect(startupCompleteId);
+                }
+            );
+        }
+        else Main.overview.showApps();
     }
 }
