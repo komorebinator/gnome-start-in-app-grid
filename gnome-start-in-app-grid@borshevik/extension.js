1dc53a145ee2503f4b0a6a751acf9cc532a7177a
komorebi <markovs.i.mail@gmail.com>
2025-12-07 06:54:52 +0400

    Try to wait till gnomeshell is loaded

--- gnome-start-in-app-grid@borshevik/extension.js
+++ gnome-start-in-app-grid@borshevik/extension.js
@@ -4,10 +4,23 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 let ranOnce = false;
 
 export default class StartInAppGridExtension extends Extension {
-    enable() {
+    show() {
         if (ranOnce) return;
+        log("Show apps");
         Main.overview.hide();
         Main.overview.showApps();
         ranOnce = true;
+    }
+    enable() {
+        if (Main.layoutManager._startingUp) {
+            let startupCompleteId = Main.layoutManager.connect(
+                "startup-complete",
+                () => {
+                    this.show();
+                    Main.layoutManager.disconnect(startupCompleteId);
+                }
+            );
+        }
+        else this.show();
     }
 }
